706aa9cb46914c198676a9e487bf28d6308cde83
abdmud <amohamud92@hotmail.com>
2023-06-12 18:48:34 +0100

    added validation functionality

--- index.js
+++ index.js
@@ -3,18 +3,28 @@ const emailInput = document.querySelector("input");
 
 function checkEmail() {
     var eMail = emailInput.value;
-
+    const emailDiv = document.querySelector(".signup");
     if (eMail != "" && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(eMail)) {
-        console.log(eMail);
+
         return true;
     } else {
         console.log("FAIL")
         emailInput.style.borderColor = "red";
+
+        // add validation message
+        let msg = document.createElement("p");
+        msg.innerHTML = "Please provide a valid email address";
+        emailDiv.append(msg);
+        // style message
+        msg.style.color = "red";
+        msg.style.fontSize = "12px";
+        msg.style.fontStyle = "italic"
+        msg.style.marginLeft = "5%"
+
         return false;
     }
 }
 
 notifyBtn.addEventListener("click", () =>{
-    console.log("button clicked!")
     checkEmail()
 })
